5e5996835e4204eeedd8ed73acde738170029fec
twio142 <64556708+twio142@users.noreply.github.com>
2024-11-09 07:31:39 +0100

    .

--- dot_config/tmux/scripts/executable_store_sessions.sh
+++ dot_config/tmux/scripts/executable_store_sessions.sh
@@ -1,14 +1,8 @@
 #!/bin/zsh
 
 # store nvim sessions
-find $TMPDIR -type s -name 'nvim.*' &> /dev/null | while read -r socket; do
-  pid=$(echo ${socket:t:r} | grep -oE '\d+')
-  ps -p $pid &> /dev/null || continue
-  ~/.local/bin/python3 <<EOF
-import pynvim
-nvim = pynvim.attach('socket', path='$socket')
-nvim.command('SessionManager save_current_session')
-EOF
+nvr --serverlist | while read -r server; do
+  nvr --servername $server --nostart -c 'SessionManager save_current_session'
 done
 
 # store tmux sessions
