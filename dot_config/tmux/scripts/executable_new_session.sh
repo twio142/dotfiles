a54f878b062cf4255be061006bbc02dbba58cbae
twio142 <64556708+twio142@users.noreply.github.com>
2024-11-16 18:18:45 +0100

    manage tmux session name

--- dot_config/tmux/scripts/executable_new_session.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-i=0
-while [ $i -lt 10 ]; do
-  tmux has -t $i 2>/dev/null || { tmux new -s $i; return; }
-  i=$((i+1))
-done
-tmux new;
--- dot_config/tmux/scripts/executable_on_session_created.sh
+++ dot_config/tmux/scripts/executable_on_session_created.sh
@@ -8,4 +8,11 @@ if [ -n "$SSH_CONNECTION" ]; then
   tmux set @ssh_connection "yes"
   tmux set -g clipboard on
   tmux has -t ssh 2> /dev/null || tmux rename ssh
+else
+  sess=$(tmux display-message -p '#S')
+  if [[ "$sess" =~ '^[0-9]+$' && "$sess" -gt 0 ]]; then
+    for i in $(seq 1 $sess); do
+      tmux has -t $i 2> /dev/null || { tmux rename $i; break; }
+    done
+  fi
 fi
