ddf67b5710db8226effb89ecef6b1d606c1ed366
twio142 <64556708+twio142@users.noreply.github.com>
2025-02-13 04:24:45 +0100

    feat!(yazi): adapt to yazi v25
    new approach to launch yazi in tmux popup: through nvim

--- dot_config/tmux/scripts/executable_open_path.sh
+++ dot_config/tmux/scripts/executable_open_path.sh
@@ -11,16 +11,15 @@ export LS_COLORS="$(vivid generate one-light)"
 BOLD=$'\033[1;36m'
 OFF=$'\033[0m'
 
-cwd=$(fzf --bind "start:reload:zoxide query '' -l | awk '{ if (!seen[tolower()]++) print }' || true" \
+fzf --bind "start:reload:zoxide query '' -l | awk '{ if (!seen[tolower()]++) print }' || true" \
 --header "${BOLD}^E${OFF} explore / ${BOLD}^G${OFF} lazygit / ${BOLD}^S${OFF} split / ${BOLD}^V${OFF} vsplit" \
 --bind "change:reload:eval zoxide query {q} -l | awk '{ if (!seen[tolower()]++) print }' || true" \
 --bind "ctrl-s:execute(tmux splitw -v -l 35% -c {})+abort" \
 --bind "ctrl-v:execute(tmux splitw -h -c {})+abort" \
 --bind "ctrl-g:execute(lazygit -p {})" \
 --bind "enter:execute(tmux neww -c {})+abort" \
---bind "ctrl-e:accept" \
+--bind "ctrl-e:execute(nvim +YaziFull\\ {})" \
 --bind "ctrl-x:reload:zoxide remove {} && eval zoxide query {q} -l | awk '{ if (!seen[tolower()]++) print }' || true" \
 --disabled \
 --preview "fzf-preview {}" \
---preview-window='up,60%')
-[ -n "$cwd" ] && yazi $cwd || exit 0
+--preview-window='up,60%'
