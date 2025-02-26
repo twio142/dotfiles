896fce2557ace04821f8506d69989c22058833d0
comet <comet@multiman>
2025-02-26 19:30:42 +0100

    feat(yazi): update some keybindings; update dependencies

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -5,13 +5,32 @@ require("full-border"):setup({
 	type = ui.Border.ROUNDED,
 })
 
+THEME.git = THEME.git or {}
+THEME.git.modified_sign = ""
+THEME.git.added_sign = "󰐕"
+THEME.git.untracked_sign = ""
+THEME.git.deleted_sign = "✖"
+THEME.git.ignored_sign = ""
 require("git"):setup()
 
-require("githead"):setup()
+require("githead"):setup({
+	branch_prefix = "",
+	branch_borders = "",
+	branch_symbol = "",
+	commit_symbol = "@",
+	behind_symbol = "󱦳",
+	ahead_symbol = "󱦲",
+	stashes_symbol = "*",
+	state_symbol = "~",
+	staged_symbol = "+",
+	unstaged_symbol = "!",
+	untracked_symbol = "?",
+})
+
 
 function Linemode:size_and_mtime()
 	local year = os.date("%Y")
-	local ts = math.floor((self._file.cha.mtime or 0))
+	local ts = math.floor(self._file.cha.mtime or 0)
 	local time
 
 	if ts > 0 and os.date("%Y", ts) == year then
@@ -32,17 +51,18 @@ function Status:name()
 
 	local linked = ""
 	if h.link_to ~= nil then
-		linked = " → " .. tostring(h.link_to)
+    local home = os.getenv("HOME")
+		linked = " → " .. tostring(h.link_to):gsub("^" .. home .. "/", "~/")
 	end
 	return ui.Line(" " .. h.name .. linked)
 end
 
+-- Show ownership in status bar
 Status:children_add(function()
 	local h = cx.active.current.hovered
 	if h == nil or ya.target_family() ~= "unix" then
 		return ""
 	end
-
 	return ui.Line({
 		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
 		":",
