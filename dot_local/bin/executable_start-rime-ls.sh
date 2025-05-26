3cf452281db425a9f7d969b83cc8eea6fd094951
twio142 <64556708+twio142@users.noreply.github.com>
2025-05-26 05:39:05 +0200

    chore(bin): simplify script

--- dot_local/bin/executable_start-rime-ls.sh
+++ dot_local/bin/executable_start-rime-ls.sh
@@ -2,5 +2,4 @@
 
 # symlink to /usr/local/bin/start-rime-ls
 
-export DYLD_LIBRARY_PATH=/opt/homebrew/lib
-exec /usr/local/bin/rime_ls --listen 127.0.0.1:9257
+DYLD_LIBRARY_PATH=/opt/homebrew/lib /usr/local/bin/rime_ls --listen 127.0.0.1:9257
